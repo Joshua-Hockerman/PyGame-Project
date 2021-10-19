1f9aa2af4732edfeeb63518076ea3f5b049c6e6a
Joshua Hockerman <jhockerman96@gmail.com>
2021-10-19 16:48:51 -0500

    Created a speed up feature to the game whenever a level is cleared.

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,6 +87,8 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
@@ -129,6 +131,7 @@ class AlienInvasion:
             # Destroy existing bullets and create a new fleet
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
