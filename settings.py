62856fd717b8cf2e40798ae8237b68d761f5770a
Joshua Hockerman <jhockerman96@gmail.com>
2021-10-19 17:41:05 -0500

    The score now increases when each fleet is cleared.

--- settings.py
+++ settings.py
@@ -22,6 +22,9 @@ class Settings:
         # How quickly  the game speeds up
         self.speedup_scale = 1.1
 
+        # How quickly the score scales up
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -41,3 +44,5 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
